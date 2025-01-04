164641f90b84692ee91938da8f2ca056a678a370
Hugo Silva <raramente@gmail.com>
2025-01-04 13:03:50 +0000

    First push

---
+++ deploy.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Re-usable deployer file for WordPress projects.
+ * Deployer 8.x
+ * php version 8.1
+ * 
+ * @category Deployer
+ * @package WordPress
+ * @author Hugo Silva <hugo@hugosilva.me>
+ * @license https://opensource.org/license/mit The MIT License
+ * 
+ */
